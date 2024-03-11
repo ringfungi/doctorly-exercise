381f6b8e659393efe4f1643ca315e2a172c4be24
Guilherme Lopes <ringfungi@gmail.com>
2024-03-11 16:00:19 +0000

    Post-submission code corrections

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 # main.tf
 provider "aws" {
-  region = "REGION" # Change to relevant region
+  region = "us-east-1" # Change to relevant region
 }
 
 resource "aws_instance" "ec2_instance" {
-  ami           = "AMI"  # Change to preferred Linux image 
-  instance_type = "t2.micro"
-  key_name      = "KEY_PAIR_NAME"  # Change to relevant EC2 key pair
+  ami           = "ami-0a55ba1c20b74fc30"  # Change to preferred Linux image 
+  instance_type = "t4g.micro"
+  key_name      = "test_kp"  # Change to relevant EC2 key pair
 
   tags = {
     Name = "ansible-ec2-instance"
@@ -31,7 +31,7 @@ resource "null_resource" "run_ansible_playbook" {
   }
 
   provisioner "local-exec" {
-    command     = "ansible-playbook -i '${self.public_ip},' -u ubuntu -i ${self.public_ip}, --private-key=PRIVATE_KEY_PATH -e 'ansible_python_interpreter=/usr/bin/python3' ../ansible_docker_dotnet/site.yml"
+    command     = "ansible-playbook -i '${aws_instance.ec2_instance.public_ip},' -u ubuntu -i ${aws_instance.ec2_instance.public_ip}, --private-key=./test_kp.pem -e 'ansible_python_interpreter=/usr/bin/python3' ../ansible_docker_dotnet/site.yml"
     working_dir = "../ansible_docker_dotnet"
   }
 }
